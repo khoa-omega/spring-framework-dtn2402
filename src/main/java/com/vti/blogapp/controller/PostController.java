acfe2fb5d323509a568ea934af97f6a9afcff4e1
Khoa <nvkhoa05@gmail.com>
2024-07-27 21:30:54 +0700

    Update

--- src/main/java/com/vti/blogapp/controller/PostController.java
+++ src/main/java/com/vti/blogapp/controller/PostController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RestController
 @AllArgsConstructor
+// @CrossOrigin("*")
 public class PostController {
     private PostService postService;
 
